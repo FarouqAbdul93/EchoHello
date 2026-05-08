1a29f93bf0af9ee03036205697f8e43985e3794d
Farouq <farouqha93@gmail.com>
2026-05-08 12:13:54 +0100

    initial project setup

---
+++ BooksApi/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BooksApi
+{
+    public class Book
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+
+        [JsonPropertyName("genre")]
+        public string Genre { get; set; }
+
+        [JsonPropertyName("year")]
+        public int Year { get; set; }
+    }
+}
---
+++ BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace BooksApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "books.json");
+            var json = System.IO.File.ReadAllText(filePath);
+            var books = JsonSerializer.Deserialize<List<Book>>(json);
+            return Ok(books);
+        }
+    }
+}
